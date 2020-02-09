2b9b1f822701f5922fad1fb5816f1c023c908108
polina <polina@example.ru>
2020-02-09 22:19:42 +0300

    I'm use IoC with the help of setters

--- src/main/java/ru/java/course/SpringPractice/MusicPlayer.java
+++ src/main/java/ru/java/course/SpringPractice/MusicPlayer.java
@@ -1,14 +1,19 @@
 package ru.java.course.SpringPractice;
 
-public class MusicPlayer {
-    private Music music;
+    public class MusicPlayer {
+        private Music music;
 
-    //Iok
-    public MusicPlayer(Music music){
-    this.music = music;
-    }
+        public MusicPlayer(){}
 
-    public void playMusic(){
-        System.out.println("Playing" + music.getSong());
-    }
+        // IoC
+        public MusicPlayer(Music music) {
+            this.music = music;
+        }
+        public void setMusic(Music music){
+            this.music = music;
+        }
+
+        public void playMusic() {
+            System.out.println("Playing: " + music.getSong());
+        }
 }
--- src/main/java/ru/java/course/SpringPractice/TestSpring.java
+++ src/main/java/ru/java/course/SpringPractice/TestSpring.java
@@ -5,12 +5,16 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-          "applicationContext.xml"
+                "applicationContext.xml"
         );
-       Music music = context.getBean("musicBean" , Music.class);
-       MusicPlayer musicPlayer = new MusicPlayer(music);
-       musicPlayer.playMusic();
-       //System.out.println(.getName());
-       context.close();
+
+        // Music music = context.getBean("musicBean", Music.class);
+        // MusicPlayer musicPlayer = new MusicPlayer(music);
+
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+        musicPlayer.playMusic();
+
+        context.close();
     }
 }
