38ba1d6d02b69f84d032c37969576f19291cbdb1
polina <polina@example.ru>
2020-02-09 22:55:53 +0300

    Add some new properties

--- src/main/java/ru/java/course/SpringPractice/MusicPlayer.java
+++ src/main/java/ru/java/course/SpringPractice/MusicPlayer.java
@@ -3,6 +3,29 @@ package ru.java.course.SpringPractice;
     public class MusicPlayer {
         private Music music;
 
+        public int getVolume() {
+            return volume;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        private String name;
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+
+        private int volume;
+
+        public void setVolume(int volume) {
+            this.volume = volume;
+        }
+
+
+
         public MusicPlayer(){}
 
         // IoC
@@ -16,4 +39,6 @@ package ru.java.course.SpringPractice;
         public void playMusic() {
             System.out.println("Playing: " + music.getSong());
         }
-}
+
+
+    }
