6c08b7e11ae0d4407189715d16ec718491313d01
polina <polina@example.ru>
2020-02-11 16:15:04 +0300

    Add Collection and new music class

--- src/main/java/ru/java/course/SpringPractice/MusicPlayer.java
+++ src/main/java/ru/java/course/SpringPractice/MusicPlayer.java
@@ -1,7 +1,10 @@
 package ru.java.course.SpringPractice;
 
-    public class MusicPlayer {
-        private Music music;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MusicPlayer {
+        private List<Music> musicList = new ArrayList<>() ;
 
         public int getVolume() {
             return volume;
@@ -24,21 +27,14 @@ package ru.java.course.SpringPractice;
             this.volume = volume;
         }
 
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
+    }
 
 
-        public MusicPlayer(){}
-
-        // IoC
-        public MusicPlayer(Music music) {
-            this.music = music;
-        }
-        public void setMusic(Music music){
-            this.music = music;
-        }
-
-        public void playMusic() {
+    public void playMusicList() {
+        for (Music music : musicList) {
             System.out.println("Playing: " + music.getSong());
         }
-
-
     }
+}
--- src/main/java/ru/java/course/SpringPractice/RapMusic.java
+++ src/main/java/ru/java/course/SpringPractice/RapMusic.java
@@ -1,8 +1,8 @@
 package ru.java.course.SpringPractice;
 
-public class ClassicalMusic implements Music {
+public class  RapMusic implements  Music{
     @Override
     public String getSong() {
-        return ("   2Chellos");
+        return ("   Faravell");
     }
 }
